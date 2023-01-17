dec49a2d4dd1dc536bfe0b503827f199b937bed2
jaeyoung <jyp101400@naver.com>
2023-01-17 11:06:38 +0900

    update front

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -30,4 +30,82 @@ function PostList() {
     </>
   );
 }
+
+// import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
+];
+
+// const Item = ({title}: any) => (
+//   <View style={styles.item}>
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
+
+// const Item = TitleList.map(function (a, i) {
+//   return (
+//     <View style={styles.item}>
+//       <Text style={styles.title}>{title}</Text>
+//     </View>
+//   )
+// })
+
+// const App = () => {
+//   const renderItem = item => <Item title={item.title} />;
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList
+//         data={DATA}
+//         renderItem={renderItem}
+//         keyExtractor={item => item.id} 
+//       />
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight || 0,
+//   },
+//   item: {
+//     backgroundColor: '#f9c2ff',
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//   },
+// });
+
+
 export default PostList;
+
+
+
+{/* SafeAreaView 사용법
+<SafeAreaView style={styles.container}>
+  <FlatList
+    data={DATA} // 필수 Props
+    renderItem={renderItem} // 필수 Props
+    keyExtractor={item => item.id}
+    numColumns={2} // Multiple Columns
+    horizontal={false} // numColumns를 사용할 때 값을 false로 지정해줘야 한다.
+    ListHeaderComponent={<Header />} // Header
+  />
+</SafeAreaView> 
+*/}
